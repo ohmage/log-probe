721e4e54f2974814430e7918455fe343fc4b0c36
Cameron Ketcham <cketcham@gmail.com>
2012-12-07 16:55:28 -0800

    added log level filtering

--- src/org/ohmage/systemlog/SystemLog.java
+++ src/org/ohmage/systemlog/SystemLog.java
@@ -11,7 +11,26 @@ import android.content.Context;
  */
 public class SystemLog extends Application {
 
+    /**
+     * Different available log levels
+     * 
+     * @author cketcham
+     */
+    public enum Loglevel {
+        NONE,
+        ERROR,
+        WARNING,
+        INFO,
+        DEBUG,
+        VERBOSE
+    }
+
     public static SystemLogProbeWriter probeWriter;
+    private static Loglevel mLogLevel;
+
+    public SystemLog(String logLevel) {
+        mLogLevel = Loglevel.valueOf(logLevel.toUpperCase());
+    }
 
     public void connect(Context context) {
         probeWriter = new SystemLogProbeWriter(context);
@@ -25,27 +44,50 @@ public class SystemLog extends Application {
         }
     }
 
+    /**
+     * Checks to see if we want to log this level of messages
+     * 
+     * @param logLevel
+     * @return
+     */
+    private static boolean shouldLogMessage(Loglevel logLevel) {
+        try {
+            return logLevel.compareTo(mLogLevel) <= 0;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private static void log(Loglevel loglevel, String tag, String message) {
+        if (shouldLogMessage(loglevel))
+            probeWriter.log(loglevel.name().toLowerCase(), tag, message);
+    }
+
     public static void i(String tag, String message) {
-        probeWriter.log("info", tag, message);
+        log(Loglevel.INFO, tag, message);
     }
 
     public static void d(String tag, String message) {
-        probeWriter.log("debug", tag, message);
+        log(Loglevel.DEBUG, tag, message);
     }
 
     public static void e(String tag, String message, Exception e) {
-        probeWriter.log("error", tag, message + e.getMessage());
+        log(Loglevel.ERROR, tag, message + e.getMessage());
+    }
+
+    public static void e(String tag, String message, Throwable throwable) {
+        log(Loglevel.ERROR, tag, message + android.util.Log.getStackTraceString(throwable));
     }
 
     public static void e(String tag, String message) {
-        probeWriter.log("error", tag, message);
+        log(Loglevel.ERROR, tag, message);
     }
 
     public static void v(String tag, String message) {
-        probeWriter.log("verbose", tag, message);
+        log(Loglevel.VERBOSE, tag, message);
     }
 
     public static void w(String tag, String message) {
-        probeWriter.log("warning", tag, message);
+        log(Loglevel.WARNING, tag, message);
     }
 }
