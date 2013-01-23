5b687d6be36ed9d9665f315ee0ea1fc2b3857a3f
Cameron Ketcham <cketcham@gmail.com>
2013-01-23 00:16:15 -0500

    updated LogProbe calls to be static

--- src/org/ohmage/logprobe/LogProbe.java
+++ src/org/ohmage/logprobe/LogProbe.java
@@ -3,6 +3,11 @@ package org.ohmage.logprobe;
 
 import android.content.Context;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
 /**
  * Creates a probewriter to send data to ohmage
  * 
@@ -16,8 +21,7 @@ public class LogProbe {
      * @author cketcham
      */
     public enum Status {
-        ON,
-        OFF
+        ON, OFF
     }
 
     /**
@@ -26,32 +30,52 @@ public class LogProbe {
      * @author cketcham
      */
     public enum Loglevel {
-        NONE,
-        ERROR,
-        WARNING,
-        INFO,
-        DEBUG,
-        VERBOSE
+        NONE, ERROR, WARNING, INFO, DEBUG, VERBOSE
     }
 
     public static LogProbeWriter probeWriter;
     public static boolean logAnalytics;
     public static Loglevel logLevel;
+    private static Set<Context> writers = Collections.synchronizedSet(new HashSet<Context>());
 
-    public LogProbe(boolean logAnalytics, String logLevel) {
-        LogProbe.logAnalytics = logAnalytics;
-        LogProbe.logLevel = Loglevel.valueOf(logLevel.toUpperCase());
+    /**
+     * Returns a LogProbeWriter and sets up the connection for writing probes to
+     * ohmage. This must be called at least once before logs are written.
+     * {@link #close(Context) } should be called to prevent leaking a context
+     * object.
+     * 
+     * @param context
+     * @return
+     */
+    public static synchronized LogProbeWriter get(Context context) {
+        writers.add(context);
+        if (probeWriter == null) {
+            probeWriter = new LogProbeWriter(context.getApplicationContext());
+            probeWriter.connect();
+        }
+        return probeWriter;
     }
 
-    public void connect(Context context) {
-        probeWriter = new LogProbeWriter(context);
-        probeWriter.connect();
-    }
-
-    public void close() {
-        if (probeWriter != null) {
+    /**
+     * close should be called after the context which is writing logs is
+     * destroyed or logs are no longer going to be written.
+     * 
+     * @param context
+     */
+    public static synchronized void close(Context context) {
+        writers.remove(context);
+        if (writers.isEmpty() && probeWriter != null) {
             probeWriter.close();
             probeWriter = null;
         }
+    }
+
+    public static void setLevel(boolean logAnalytics, String logLevel) {
+        setLevel(logAnalytics, Loglevel.valueOf(logLevel.toUpperCase(Locale.US)));
+    }
+
+    public static void setLevel(boolean logAnalytics, Loglevel logLevel) {
+        LogProbe.logAnalytics = logAnalytics;
+        LogProbe.logLevel = logLevel;
     }
 }
